6bc040c94b6145ec3dfa0bdb64a1df4373741774
Isaac <fimbresisaac23@gmail.com>
2023-01-07 17:00:56 -0800

    new hobbies data

---
+++ src/data/hobbies.ts
@@ -0,0 +1,27 @@
+export const hobbies = [
+    {
+        id: "1",
+        title: "Programming",
+        description: "Using the computer to write some code"
+    },
+    {
+        id: "2",
+        title: "Bowling",
+        description: "Throw a bowling ball to knocking down pins"
+    },
+    {
+        id: "3",
+        title: "Swimming",
+        description: "Get in the water and go through it"
+    },
+    {
+        id: "4",
+        title: "Fencing",
+        description: "A hobby for fency people"
+    },
+    {
+        id: "5",
+        title: "Hiking",
+        description: "Wear boots and explore the world"
+    }
+];
