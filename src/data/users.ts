bca3bb2eaebacd7f1d2c2cbef4894a509457ab86
Isaac <fimbresisaac23@gmail.com>
2023-01-07 16:38:20 -0800

    adding users data

---
+++ src/data/users.ts
@@ -0,0 +1,27 @@
+export const users = [
+    {
+        id: "1",
+        name: "Isaac",
+        age: 22
+    },
+    {
+        id: "2",
+        name: "Andrea",
+        age: 22
+    },
+    {
+        id: "3",
+        name: "Rodolfo",
+        age: 22
+    },
+    {
+        id: "4",
+        name: "Esther",
+        age: 74
+    },
+    {
+        id: "5",
+        name: "Raul",
+        age: 32
+    },
+];
