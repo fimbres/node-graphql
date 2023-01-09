ab9a8ad0bc7ac50884c9d29c0905f83324321a62
Isaac <fimbresisaac23@gmail.com>
2023-01-08 18:46:42 -0800

    connecting to mongodb atlas cluster

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,28 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 import { rootSchema } from './schema';
-import { testSchema } from './types/schema';
+
+dotenv.config({
+    path: './.env'
+});
 
 const app = express();
+const port = process.env.PORT || 5000;
+const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.delqixp.mongodb.net/?retryWrites=true&w=majority`;
 
 app.use('/graphql', graphqlHTTP({
-    schema: testSchema,
+    schema: rootSchema,
     graphiql: true,
 }));
 
-app.listen(5000, () => {
-    console.log("Server listening on port 5000");
-});
+mongoose.set('strictQuery', false);
+mongoose.connect(uri)
+.then(() => {
+    app.listen(port, () => {
+        console.log("Server listening on port " + port);
+    });
+})
+.catch(e => console.log(e));
